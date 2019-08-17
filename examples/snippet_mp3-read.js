73a6e20e3f69c79647979d4249a259bb94299546
ffalt <ffalty@gmail.com>
2019-08-17 09:51:41 +0200

    docs(snippets): add some mp3 read demo code

---
+++ examples/snippet_mp3-read.js
@@ -0,0 +1,23 @@
+const jamp3 = require('jamp3');
+
+function run() {
+	const mp3 = new MP3();
+	const filename = 'demo.mp3';
+	const options = {
+		mpeg: true, // read mpeg information
+		mpegQuick: true, // estimate mpeg information based on mpeg header (XING|Info) and stop reading if tags and header is found
+		id3v2: true, // read ID3 v2 tag
+		id3v1: false,  // read ID3 v1 tag
+		id3v1IfNotID3v2: true,  // read ID3 v1 tag only if no ID3 v2 tag is found (stops reading otherwise)
+		raw: false // do not parse frames & return all frames as binary blobs
+	};
+	mp3.read(filename, options)
+	.then((data) => {
+		console.log('mp3:', data);
+	})
+	.catch(e => {
+		console.error(e);
+	});
+}
+
+run();
