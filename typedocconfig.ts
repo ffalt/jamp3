c8b18f10e8996dbf096ef09db1271d4970561f42
ffalt <ffalty@gmail.com>
2019-08-17 12:56:54 +0200

    docs(typedoc): add typedoc generator

---
+++ typedocconfig.ts
@@ -0,0 +1,29 @@
+module.exports = {
+	src: [
+		'./src/lib/mp3/mp3.ts',
+		'./src/lib/id3v2/id3v2.ts',
+		'./src/lib/id3v1/id3v1.ts',
+		'./src/lib/mp3/mp3__types.ts',
+		'./src/lib/mp3/mp3_analyzer.ts',
+		'./src/lib/id3v2/id3v2__types.ts',
+		'./src/lib/id3v2/id3v2_builder24.ts',
+		'./src/lib/id3v2/id3v2_simplify.ts',
+		'./src/lib/id3v1/id3v1__types.ts'
+	],
+	includes: [
+		'./examples/'
+	],
+	mode: 'file',
+	includeDeclarations: true,
+	tsconfig: 'tsconfig.json',
+	out: './docs',
+	theme: 'minimal',
+	excludePrivate: true,
+	excludeProtected: true,
+	excludeExternals: true,
+	readme: 'README.md',
+	name: 'jamp3',
+	ignoreCompilerErrors: true,
+	plugin: 'none',
+	listInvalidSymbolLinks: true,
+};
