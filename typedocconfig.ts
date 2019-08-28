accc7ccf16474ade20f5f1062c26119d6aced8e9
ffalt <ffalty@gmail.com>
2019-08-28 19:05:21 +0200

    docs(ID3v2): include constants

--- typedocconfig.ts
+++ typedocconfig.ts
@@ -5,6 +5,7 @@ module.exports = {
 		'./src/lib/id3v1/id3v1.ts',
 		'./src/lib/mp3/mp3__types.ts',
 		'./src/lib/mp3/mp3_analyzer.ts',
+		'./src/lib/id3v2/id3v2__consts.ts',
 		'./src/lib/id3v2/id3v2__types.ts',
 		'./src/lib/id3v2/id3v2_builder24.ts',
 		'./src/lib/id3v1/id3v1__types.ts'
