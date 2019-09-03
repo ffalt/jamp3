546beddc7ec89a71597ec803faa456c8c40c2311
ffalt <ffalty@gmail.com>
2019-09-03 17:21:54 +0200

    refactor(code climate): reduce duplication

--- src/lib/id3v2/id3v2.ts
+++ src/lib/id3v2/id3v2.ts
@@ -1,9 +1,10 @@
 import fse from 'fs-extra';
+import {Readable} from 'stream';
+
 import {ID3v2Reader} from './id3v2.reader';
 import {ID3v2Writer} from './id3v2.writer';
 import {IID3V2} from './id3v2.types';
 import {fileRangeToBuffer} from '../common/utils';
-import {Readable} from 'stream';
 import {updateFile} from '../common/update-file';
 import {ITagID} from '../common/types';
 import {rawHeaderOffSet} from '../mp3/mp3.mpeg.frame';
@@ -12,6 +13,7 @@ import {simplifyTag} from './id3v2.simplify';
 import {FileWriterStream} from '../common/stream-writer-file';
 import {writeRawFrames} from './frames/id3v2.frame.write';
 import {buildID3v2} from './frames/id3v2.frame.read';
+import {IMP3} from '../mp3/mp3.types';
 
 /**
  * Class for
@@ -94,24 +96,7 @@ export class ID3v2 {
 	async remove(filename: string, options: IID3V2.RemoveOptions): Promise<boolean> {
 		let removed = false;
 		await updateFile(filename, {id3v2: true, mpegQuick: true}, !!options.keepBackup, () => true, async (layout, fileWriter): Promise<void> => {
-			let start = 0;
-			let specEnd = 0;
-			for (const tag of layout.tags) {
-				if (tag.id === ITagID.ID3v2) {
-					if (start < tag.end) {
-						specEnd = (tag as IID3V2.RawTag).head.size + tag.start + 10 /*header itself*/;
-						start = tag.end;
-						removed = true;
-					}
-				}
-			}
-			if (layout.frameheaders.length > 0) {
-				const mediastart = rawHeaderOffSet(layout.frameheaders[0]);
-				start = specEnd < mediastart ? specEnd : mediastart;
-			} else {
-				start = Math.max(start, specEnd);
-			}
-			await fileWriter.copyFrom(filename, start);
+			removed = await this.copyAudio(filename, layout, fileWriter);
 		});
 		return removed;
 	}
@@ -178,27 +163,34 @@ export class ID3v2 {
 		await stream.close();
 	}
 
+	private async copyAudio(filename: string, layout: IMP3.RawLayout, fileWriter: FileWriterStream): Promise<boolean> {
+		let start = 0;
+		let specEnd = 0;
+		let skipped = false;
+		for (const tag of layout.tags) {
+			if (tag.id === ITagID.ID3v2) {
+				if (start < tag.end) {
+					specEnd = (tag as IID3V2.RawTag).head.size + tag.start + 10 /*header itself*/;
+					start = tag.end;
+					skipped = true;
+				}
+			}
+		}
+		if (layout.frameheaders.length > 0) {
+			const mediastart = rawHeaderOffSet(layout.frameheaders[0]);
+			start = specEnd < mediastart ? specEnd : mediastart;
+		} else {
+			start = Math.max(start, specEnd);
+		}
+		await fileWriter.copyFrom(filename, start);
+		return skipped;
+	}
+
 	private async replaceTag(filename: string, frames: Array<IID3V2.RawFrame>, head: IID3V2.TagHeader, options: IID3V2.WriteOptions): Promise<void> {
 		await updateFile(filename, {id3v2: true, mpegQuick: true}, !!options.keepBackup, () => true, async (layout, fileWriter): Promise<void> => {
 			const writer = new ID3v2Writer();
 			await writer.write(fileWriter, frames, head, options);
-			let start = 0;
-			let specEnd = 0;
-			for (const tag of layout.tags) {
-				if (tag.id === ITagID.ID3v2) {
-					if (start < tag.end) {
-						specEnd = (tag as IID3V2.RawTag).head.size + tag.start + 10 /*header itself*/;
-						start = tag.end;
-					}
-				}
-			}
-			if (layout.frameheaders.length > 0) {
-				const mediastart = rawHeaderOffSet(layout.frameheaders[0]);
-				start = specEnd < mediastart ? specEnd : mediastart;
-			} else {
-				start = Math.max(start, specEnd);
-			}
-			await fileWriter.copyFrom(filename, start);
+			await this.copyAudio(filename, layout, fileWriter);
 		});
 	}
 
