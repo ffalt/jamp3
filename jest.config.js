43e62b2b7fc8cbcf489fafec2f66cfb7131a4ca5
ffalt <ffalty@gmail.com>
2019-12-06 14:12:01 +0100

    test(jest): add ignore paths

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
 	preset: "ts-jest",
 	testEnvironment: "node",
 	coverageDirectory: "coverage",
+	modulePathIgnorePatterns: ['<rootDir>/local/', '<rootDir>/examples', '<rootDir>/dist', '<rootDir>/docs', '<rootDir>/bin'],
 	testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(test).[jt]s?(x)"]
 };
