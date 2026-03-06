03745697fb4de94862a4eea01c102d8421ce7c09
ffalt <ffalty@gmail.com>
2026-03-06 17:08:27 +0100

    fix: reorder ID3v2 test directories for consistency

--- test/id3v2/id3v2.config.ts
+++ test/id3v2/id3v2.config.ts
@@ -3,16 +3,17 @@ import path from 'node:path';
 export const ID3v2TestPath = path.join(__dirname, '..', 'data', 'testfiles', 'id3v2');
 export const ID3v2TestDirectories = [
 	// 'bla',
-	'getID3',
-	'various',
+	'ID3Edit',
 	'agrundma',
+	'getID3',
 	'helios',
 	'id3-go',
 	'id3-parser',
-	'ID3Edit',
 	'mp3agic',
+	'mutagen',
 	'rust-id3',
 	'sqell-id3',
 	'stagger',
-	'taglib'
+	'taglib',
+	'various'
 ];
